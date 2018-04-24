3c51c251095d46fa0f3ae26b9416d95cf7f1db55
Danuta <danprzy@gmail.com>
2018-04-24 22:37:03 +0200

    Add script for warranty cost

--- scripts.js
+++ scripts.js
@@ -9,6 +9,10 @@ Phone.prototype.printInfo = function() {
 		console.log("The phone brand is " + this.brand + ", color is " + this.color + " and the price is " + this.price + ".");
 }
 
+Phone.prototype.getWarranyCost = function() {
+	console.log(this.brand + ' warranty price is: ' + this.price * 0.1 + ".");
+}
+
 var SamsungGalaxyS6 = new Phone("Samsung", 1400, "black");
 var iPhone6S = new Phone("Apple", 2250, "silver");
 var OnePlusOne = new Phone("OnePlus", 1500, "black");
@@ -16,3 +20,7 @@ var OnePlusOne = new Phone("OnePlus", 1500, "black");
 SamsungGalaxyS6.printInfo();
 iPhone6S.printInfo();
 OnePlusOne.printInfo();
+
+SamsungGalaxyS6.getWarranyCost();
+iPhone6S.getWarranyCost();
+OnePlusOne.getWarranyCost();
