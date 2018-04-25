197afa8292707af5250a69ab5c06274cdf0b8b39
Danuta <danprzy@gmail.com>
2018-04-25 15:55:37 +0200

    Change method for warranty cost

--- scripts.js
+++ scripts.js
@@ -3,15 +3,15 @@ function Phone(brand, price, color) {
 	//by using this, the "brand" property of the object we create will assume the value of the brand argument
 	this.price = price;
 	this.color = color;
-}
+};
 
 Phone.prototype.printInfo = function() {
-		console.log("The phone brand is " + this.brand + ", color is " + this.color + " and the price is " + this.price + ".");
-}
+		console.log("The phone brand is " + this.brand + ", color is " + this.color + " and the price is " + this.price + ', warranty cost is ' + this.getWarranyCost() + ".");
+};
 
 Phone.prototype.getWarranyCost = function() {
-	console.log(this.brand + ' warranty price is: ' + this.price * 0.1 + ".");
-}
+	return this.price * 0.1;
+};
 
 var SamsungGalaxyS6 = new Phone("Samsung", 1400, "black");
 var iPhone6S = new Phone("Apple", 2250, "silver");
@@ -20,7 +20,3 @@ var OnePlusOne = new Phone("OnePlus", 1500, "black");
 SamsungGalaxyS6.printInfo();
 iPhone6S.printInfo();
 OnePlusOne.printInfo();
-
-SamsungGalaxyS6.getWarranyCost();
-iPhone6S.getWarranyCost();
-OnePlusOne.getWarranyCost();
